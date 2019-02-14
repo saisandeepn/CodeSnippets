53739b7e0a56a75c141507fc839a54a2208ae32c
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2019-02-13 23:29:11 -0500

    Spring Multi Module Setup
    Basic Spring Multi Module Setup

---
+++ springboot-multi-module-parent/persistence/src/main/java/com/sai/sandeep/persistence/App.java
@@ -0,0 +1,13 @@
+package com.sai.sandeep.persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
